06c58241ee7b26308a6fb91a9033232cbb382df3
Kyle Houghton <kylebenz@hotmail.com>
2018-12-30 00:20:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,13 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      attribute.each do |attribute_property|
+        if attribute_property == "strawberry"
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
